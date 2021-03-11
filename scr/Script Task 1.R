f6a11ae91d38acb1ae82a42937b8e754177b99d5
gabriela-navarro-m <g.navarrom@uniandes.edu.co>
2021-03-11 18:34:50 -0500

    espacios entre iguales por etica de programador

--- scr/Script Task 1.R
+++ scr/Script Task 1.R
@@ -7,13 +7,13 @@
 #1. VECTORES
 
   # Vector 1 que contiene los números del 1 al 100
-  vector1=c(1:100) 
+  vector1 = c (1:100) 
 
   # Vector 2 que contiene numeros impares del 1 al 99
-  vector2=seq(from=1, to=99, by=2 ) #Se establece una secuencia donde empieza por el numero 1 y termina en el 99 con saltos de dos numeros para tener solo impares
+  vector2 = seq (from=1, to=99, by=2 ) #Se establece una secuencia donde empieza por el numero 1 y termina en el 99 con saltos de dos numeros para tener solo impares
 
   # Vector 3 que contiene numeros pares del vector 1 
-  vector3=vector1[-vector2] #Se usa vector1[] para seleccionar elementos de ese vector, luego se usa como argumento -vector2, pues al tener el signo negativo R devuelve todos los numeros del vector1 sin contar los del vector2
+  vector3 = vector1 [-vector2] #Se usa vector1[] para seleccionar elementos de ese vector, luego se usa como argumento -vector2, pues al tener el signo negativo R devuelve todos los numeros del vector1 sin contar los del vector2
 
 #2. LIMPIAR BASE DE DATOS
 
@@ -22,15 +22,15 @@
   pacman::p_load(here,tidyverse,reshape2) #Cargar y/o instalar paquetes requeridos
   
   #Para importar la base de datos se usa read_xlsx porque el archivo es de Excel
-  cultivos=read_xlsx("data/input/cultivos.xlsx")
-  view(cultivos) #para ver la base de datos 
+  cultivos = read_xlsx("data/input/cultivos.xlsx")
+  view (cultivos) #para ver la base de datos 
   
   #Para crear una nueva base de datos donde se eligan las variables que se van a utilizar: Municipios y el resto de años. Además, se eligen desde la fila 5 porque el inicial son espacios nulos
   basecultivos = cultivos[5:nrow(cultivos),] %>% dplyr::select(.,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25) 
   view (basecultivos)
   
   #Se cambian los nombres de las columnas para poder identificarlos al hacer el pivoteo
-  colnames(basecultivos) = c("MUNICIPIO", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019")
+  colnames (basecultivos) = c("MUNICIPIO", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019")
   view (basecultivos)
   
   #Se descartan los espacios nulos
